c0490dd671376d88cdcaf54adf6171d029906efb
ark dan <arkdan@icloud.com>
2016-11-16 00:11:55 +0200

    initial

---
+++ XPCMessaging/XPCMessaging.h
@@ -0,0 +1,19 @@
+//
+//  XPCMessaging.h
+//  XPCMessaging
+//
+//  Created by ark dan on 11/16/16.
+//  Copyright © 2016 arkdan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for XPCMessaging.
+FOUNDATION_EXPORT double XPCMessagingVersionNumber;
+
+//! Project version string for XPCMessaging.
+FOUNDATION_EXPORT const unsigned char XPCMessagingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XPCMessaging/PublicHeader.h>
+
+
